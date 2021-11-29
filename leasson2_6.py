97e452e58e1cc2d14fab2bce9e1eaf9eefefda1f
dmitrycd838 <churilov.dmitrycd@mail.ru>
2021-11-29 16:19:39 +0300

    leas_2

---
+++ leasson2_6.py
@@ -0,0 +1,44 @@
+# 6.* Реализовать структуру данных «Товары». Она должна представлять собой список кортежей.
+# Каждый кортеж хранит информацию об отдельном товаре.
+# В кортеже должно быть два элемента — номер товара и словарь с параметрами
+# (характеристиками товара: название, цена, количество, единица измерения).
+# Структуру нужно сформировать программно, т.е. запрашивать все данные у пользователя.
+
+goods = []
+features = {'имя': '', 'цена': '', 'количество': '', 'ед': ''}
+analytics = {'имя': [], 'цена': [], 'количество': [], 'ед': []}
+num = 0
+feature_ = None
+control = None
+while True:
+    control = input("Для выхода нажмите 'Q', Для продолжения 'Enter', Для аналитики 'A'").upper()
+    if control == 'Q':
+        break
+    num += 1
+    if control == 'A':
+        print(f'\n Current analytics \n {"-" * 30}')
+        for key, value in analytics.items():
+            print(f'{key[:25]:>30}: {value}')
+            print("-" * 30)
+    for f in features.keys():
+        feature_ = input(f'Input feature "{f}"')
+        features[f] = int(feature_) if (f == 'price' or f == 'quantity') else feature_
+        analytics[f].append(features[f])
+    goods.append((num, features))
+
+
+goods = int(input("Введите количество товара "))
+n = 1
+my_dict = []
+my_list = []
+my_analys = {}
+while n <= goods:
+    my_dict = dict({'Название': input("Введите название: "), 'цена': input("Введите цену: "),
+                    'Количество': input('Введите количество: '), 'eд': input("Введите единицу измерения: ")})
+    my_list.append((n, my_dict))
+    n += 1
+    my_analys = dict(
+        {'название': my_dict.get('название'), 'цена': my_dict.get('цена'), 'количество': my_dict.get('количество'),
+         'ед': my_dict.get('ед')})
+print(my_list)
+print(my_analys)
